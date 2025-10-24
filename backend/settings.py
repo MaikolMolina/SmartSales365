71da58e37a6a4dfc665d9b77a4e1ca71076a1f3d
MaikolMolina <MolinaMaikol73@gmail.com>
2025-10-24 08:57:45 -0400

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -70,10 +70,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "backend", "templates")],
+        'DIRS': [
+            os.path.join(BASE_DIR, "backend", "templates"),
+            os.path.join(BASE_DIR, "frontend", "build"),  
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
