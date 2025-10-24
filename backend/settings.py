427439e3ebb71bb83d86b720596729f671ffae4c
MaikolMolina <MolinaMaikol73@gmail.com>
2025-10-24 07:35:20 -0400

    adaptación settings a render

--- backend/settings.py
+++ backend/settings.py
@@ -70,7 +70,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "staticfiles")],
+        'DIRS': [os.path.join(BASE_DIR, "backend", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
